1f3cb3d4fe29264eb62ba67c1da4edf6b795cbb4
FlyDog-Hub <raojiaming04@gmail.com>
2025-08-09 22:41:05 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -1,8 +1,9 @@
 # 添加包
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki --depth=1 clone/nikki
 
 # 升级包
-cp -rf clone/amlogic/luci-app-amlogic  feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic clone/nikki/luci-app-nikki  feeds/luci/applications/
 
 # 清理包
 rm -rf clone
